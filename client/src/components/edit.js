35e3ed0d63411df588e5e3c0623a3ce169a4563c
Anthony Berton <anthonyberton@Anthonys-MacBook-Air.local>
2022-04-12 08:23:26 -0700

    first commit

---
+++ client/src/components/edit.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router";
+
+export default function Edit() {
+  const [form, setForm] = useState({
+    name: "",
+    position: "",
+    level: "",
+    certifications: "",
+    records: [],
+  });
+  const params = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchData() {
+      const id = params.id.toString();
+      const response = await fetch(`http://localhost:5000/record/${params.id.toString()}`);
+
+      if (!response.ok) {
+        const message = `An error has occured: ${response.statusText}`;
+        window.alert(message);
+        return;
+      }
+
+      const record = await response.json();
+      if (!record) {
+        window.alert(`Record with id ${id} not found`);
+        navigate("/");
+        return;
+      }
+
+      setForm(record);
+    }
+
+    fetchData();
+
+    return;
+  }, [params.id, navigate]);
+
+  // These methods will update the state properties.
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    const editedPerson = {
+      name: form.name,
+      position: form.position,
+      level: form.level,
+      certifications: form.certifications,
+      cni_email: form.cni_email,
+    };
+
+    // This will send a post request to update the data in the database.
+    await fetch(`http://localhost:5000/update/${params.id}`, {
+      method: "POST",
+      body: JSON.stringify(editedPerson),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    navigate("/");
+  }
+
+  // This following section will display the form that takes input from the user to update the data.
+  return (
+    <div>
+      <h3>Update Record</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={form.name}
+            onChange={(e) => updateForm({ name: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="position">Department/Program: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="position"
+            value={form.position}
+            onChange={(e) => updateForm({ position: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="cni_email">CNI Email: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="position"
+            value={form.cni_email}
+            onChange={(e) => updateForm({ cni_email: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="certifications">Documentations: </label>
+          <input
+            type="text"
+            className="form-control"
+            id="certifications"
+            value={form.certifications}
+            onChange={(e) => updateForm({ certifications: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionFaculty"
+              value="Faculty"
+              checked={form.level === "Faculty"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionFaculty" className="form-check-label">Faculty</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionStaff"
+              value="Staff"
+              checked={form.level === "Staff"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionStaff" className="form-check-label">Staff</label>
+          </div>
+          <div className="form-check form-check-inline">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="positionOptions"
+              id="positionStudent"
+              value="Student"
+              checked={form.level === "Student"}
+              onChange={(e) => updateForm({ level: e.target.value })}
+            />
+            <label htmlFor="positionStudent" className="form-check-label">Student</label>
+          </div>
+        </div>
+        <br />
+
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Update Record"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+}
